5e83fd6f66e365827c0ecda064cd809a83939130
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2015-09-30 18:24:11 +0100

    Refactored code using a struct to encapsulate encoder data.
    As part of this, added two built-in encodings and a factory function:
    
    * DefaultEncoding is the encoding previously used by this library and
      has two methods renamed globally for compatibility reasons.
    * StdEncoding is the encoding used by Python's base58 package and, IIRC,
      bitcoin and others.
    
    The encoder structs have two new methods that accept byte-strings and
    emit byte-strings, also.
    
    Tests pass but none have been added to test new functionality, yet.

--- base58.go
+++ base58.go
@@ -10,33 +10,63 @@ import (
 	"strconv"
 )
 
-//const alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
-const alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+const caps_last_alphabet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
+const caps_first_alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
-var decodeMap [256]byte
+// Uses alphabet "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
+// Default for compatibility reasons. The "EncodeBig" and "DecodeToBig"
+// top-level functions are just shortcuts for methods on this encoding.
+var DefaultEncoding = NewEncoder(caps_last_alphabet)
 
-func init() {
-	for i := 0; i < len(decodeMap); i++ {
-		decodeMap[i] = 0xFF
-	}
-	for i := 0; i < len(alphabet); i++ {
-		decodeMap[alphabet[i]] = byte(i)
-	}
+// Uses alphabet "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+// Standard because it appears to be in wider use, v/v miniLock, Bitcoin,
+// Python's base58, etc.
+var StdEncoding = NewEncoder(caps_first_alphabet)
+
+// Encode encodes src, appending to dst. Be sure to use the returned
+// new value of dst.
+// This is a shortcut to DefaultEncoding.EncodeBig. It's suggested for
+// maintainability reasons to use the full reference in new code.
+var EncodeBig func([]byte, *big.Int)[]byte = DefaultEncoding.EncodeBig
+
+// Decode a big integer from the bytes. Returns an error on corrupt
+// input.
+// This is a shortcut to DefaultEncoding.DecodeToBig. It's suggested for
+// maintainability reasons to use the full reference in new code.
+var DecodeToBig func([]byte)(*big.Int,error) = DefaultEncoding.DecodeToBig
+
+// Functions normally global re-implemented as shortcuts to library-level
+// encodingAlphabets, mimicing the behaviour of base64/base32 and providing
+// more flexibility while retaining backwards compatibility.
+// Users can define their own alphabets, two are provided.
+type encodingAlphabet struct{
+	alphabet string
+	decodeMap [256]byte
 }
 
-type CorruptInputError int64
-
-func (e CorruptInputError) Error() string {
-	return "illegal base58 data at input byte " + strconv.FormatInt(int64(e), 10)
+// If a custom alphabet is desired it can be defined here by passing a length 62
+// string of unique characters. Two encodings are provided in this library already,
+// representing "caps first" or "caps last" base58.
+func NewEncoder(alphabet string) *encodingAlphabet {
+  enc := new(encodingAlphabet)
+ 	enc.alphabet = alphabet
+  // Is this necessary after `new`?
+	for i := 0; i < len(enc.decodeMap); i++ {
+		enc.decodeMap[i] = 0xFF
+	}
+	for i := 0; i < len(enc.alphabet); i++ {
+		enc.decodeMap[enc.alphabet[i]] = byte(i)
+	}
+  return enc
 }
 
 // Decode a big integer from the bytes. Returns an error on corrupt
 // input.
-func DecodeToBig(src []byte) (*big.Int, error) {
+func (self *encodingAlphabet) DecodeToBig(src []byte) (*big.Int, error) {
 	n := new(big.Int)
 	radix := big.NewInt(58)
 	for i := 0; i < len(src); i++ {
-		b := decodeMap[src[i]]
+		b := self.decodeMap[src[i]]
 		if b == 0xFF {
 			return nil, CorruptInputError(i)
 		}
@@ -48,7 +78,7 @@ func DecodeToBig(src []byte) (*big.Int, error) {
 
 // Encode encodes src, appending to dst. Be sure to use the returned
 // new value of dst.
-func EncodeBig(dst []byte, src *big.Int) []byte {
+func (self *encodingAlphabet) EncodeBig(dst []byte, src *big.Int) []byte {
 	start := len(dst)
 	n := new(big.Int)
 	n.Set(src)
@@ -58,7 +88,7 @@ func EncodeBig(dst []byte, src *big.Int) []byte {
 	for n.Cmp(zero) > 0 {
 		mod := new(big.Int)
 		n.DivMod(n, radix, mod)
-		dst = append(dst, alphabet[mod.Int64()])
+		dst = append(dst, self.alphabet[mod.Int64()])
 	}
 
 	for i, j := start, len(dst)-1; i < j; i, j = i+1, j-1 {
@@ -67,15 +97,23 @@ func EncodeBig(dst []byte, src *big.Int) []byte {
 	return dst
 }
 
-func b58Encode(data []byte) []byte {
+// Encode a byte string to binary base58 using this encoding.
+func (self *encodingAlphabet) Encode(data []byte) []byte {
   bigInt := big.NewInt(0).SetBytes(data)
-  return EncodeBig(nil, bigInt)
+  return self.EncodeBig(nil, bigInt)
 }
 
-func b58Decode(enc []byte) ([]byte, error) {
-  bigInt, err := DecodeToBig(enc)
+// Decode binary base58 to a byte string using this encoding.
+func (self *encodingAlphabet) Decode(enc []byte) ([]byte, error) {
+  bigInt, err := self.DecodeToBig(enc)
   if err != nil {
     return nil, err
   }
   return bigInt.Bytes(), nil
+}
+
+type CorruptInputError int64
+
+func (e CorruptInputError) Error() string {
+	return "illegal base58 data at input byte " + strconv.FormatInt(int64(e), 10)
 }
